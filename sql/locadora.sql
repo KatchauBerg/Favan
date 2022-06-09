24fefb98d6922469356677e7d2b365b5eca80186
KatchauBerg <kevinlima138@gmail.com>
2022-06-09 17:22:07 -0300

    subindo novo arquivo sql

--- sql/locadora.sql
+++ sql/locadora.sql
@@ -1,5 +1,7 @@
-CREATE SCHEMA locadora;
 USE locadora;
+DROP TABLE registroalugado;
+DROP TABLE filme;
+DROP TABLE usuario;
 
 CREATE TABLE usuario(
 	id INT NOT NULL AUTO_INCREMENT,
@@ -9,21 +11,20 @@ CREATE TABLE usuario(
     
     PRIMARY KEY (id)
 );
-
 CREATE TABLE filme (
 	id INT NOT NULL AUTO_INCREMENT,
     nomeFilme VARCHAR(100) NOT NULL,
     categoria VARCHAR(100) NOT NULL,
+    estado ENUM("A","D") NOT NULL,
     
     PRIMARY KEY (id)
 );
 
 CREATE TABLE registroAlugado(
-	id INT NOT NULL AUTO_INCREMENT,
     id_usuario INT NOT NULL,
     id_filme INT NOT NULL,
     
-    PRIMARY KEY (id),
+    CONSTRAINT usuario_filme_pk PRIMARY KEY (id_usuario,id_filme),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id),
     FOREIGN KEY (id_filme) REFERENCES filme(id)
 );
